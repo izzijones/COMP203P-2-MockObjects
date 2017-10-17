2041b08b26db5ec330b6821d9ca2cc33b164eaa0
izzijones <izzijones@me.com>
2017-10-17 16:51:21 +0100

    implemented Camera and tests

--- src/main/java/ucl/cs/camera/Camera.java
+++ src/main/java/ucl/cs/camera/Camera.java
@@ -1,17 +1,46 @@
 package ucl.cs.camera;
 
 public class Camera {
+  private Sensor sensor;
+  private MemoryCard memoryCard;
+  private Boolean on = false;
+  private Boolean writing = false;
+  private WriteListener writeListener = new WriteListener() {
+      @Override
+      public void writeComplete() {
+          writing = false;
+          notify();
+      }
+  };
+
+  public Camera(Sensor s, MemoryCard m){
+    sensor = s;
+    memoryCard = m;
+  }
 
   public void pressShutter() {
-    // not implemented
+    if(on){
+      writing = true;
+      memoryCard.write(sensor.readData());
+      writeListener.writeComplete();
+      sensor.powerDown();
+    }
   }
 
   public void powerOn() {
-    // not implemented
+    on = true;
+    sensor.powerUp();
   }
 
-  public void powerOff() {
-    // not implemented
+  public void powerOff() throws InterruptedException {
+    on = false;
+    if (!writing){
+      sensor.powerDown();
+    }
+    else{
+        wait();
+        sensor.powerDown();
+    }
   }
 }
 
--- src/test/java/ucl/cs/camera/CameraTest.java
+++ src/test/java/ucl/cs/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package ucl.cs.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,10 +10,63 @@ public class CameraTest {
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
+  Sensor sensor = context.mock(Sensor.class);
+  MemoryCard memoryCard  = context.mock(MemoryCard.class);
+
+  Camera camera = new Camera(sensor, memoryCard);
+
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
 
-    // write your test here
+    context.checking(new Expectations(){{
+      exactly(1).of(sensor).powerUp();
+    }});
+
+    camera.powerOn();
   }
+
+  @Test
+  public void switchingCameraOffWhileNotWriting(){
+
+    context.checking(new Expectations(){{
+      exactly(1).of(sensor).powerDown();
+    }});
+
+    camera.powerOff();
+  }
+
+  @Test
+  public void pressingShutterWhileOffDoesNothing(){
+
+    context.checking(new Expectations(){{
+      exactly(1).of(sensor).powerDown();
+      exactly(0).of(sensor).readData();
+    }});
+    camera.powerOff();
+    camera.pressShutter();
+  }
+
+  @Test
+  public void pressingShutterWhileOnCopiesData(){
+    context.checking(new Expectations(){{
+      exactly(1).of(sensor).readData(); will(returnValue(new byte[0]));
+      exactly(1).of(sensor).powerUp();
+      exactly(1).of(memoryCard).write(new byte[0]);
+      exactly(1).of(sensor).powerDown();
+    }});
+
+    camera.powerOn();
+    camera.pressShutter();
+  }
+
+  public void turningCameraOffDoesNotInterruptWrite(){
+    context.checking(new Expectations(){{
+      exactly(1).of(sensor).powerDown();
+    }});
+    camera.pressShutter();
+    camera.powerOff();
+  }
+
+
 
 }
